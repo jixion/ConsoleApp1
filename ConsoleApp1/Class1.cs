f949ef3a4b4dadbc4d46d90204fbc1eb120fa464
Jeff Burroughs <jburroughs@sparkimaginations.com>
2021-09-25 13:31:48 -0500

    Initial commit

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListImplementations
+{
+    internal class SinglyNode
+    {
+        internal string data;
+        internal SinglyNode next;
+
+        public SinglyNode()
+        {
+            data = "Glagnar's human rinds are a buncha cruncha human in every bite!";
+            next = null;
+        }
+        public SinglyNode(string d)
+        {
+            this.data = d;
+            next = null;
+        }
+    }
+
+    internal class DoublyNode
+    {
+        internal string data;
+        internal DoublyNode prev;
+        internal DoublyNode next;
+
+        public DoublyNode()
+        {
+            data = null;
+            prev = null;
+            next = null;
+        }
+
+        public DoublyNode(string d)
+        {
+            data = d;
+            prev = null;
+            next = null;
+        }
+    }
+
+    internal class SinglyLinkedList
+    {
+        internal SinglyNode head;
+
+        internal void InsertLast(SinglyLinkedList singlyList)
+        {
+            SinglyNode newNode = new SinglyNode();
+            if (singlyList.head == null)
+            {
+                singlyList.head = newNode;
+                return;
+            }
+            SinglyNode lastNode = GetLastNode(singlyList);
+            lastNode.next = newNode;
+        }
+        internal void InsertLast(SinglyLinkedList singlyList, string newData)
+        {
+            SinglyNode newNode = new SinglyNode(newData);
+            if (singlyList.head == null)
+            {
+                singlyList.head = newNode;
+                return;
+            }
+            SinglyNode lastNode = GetLastNode(singlyList);
+            lastNode.next = newNode;
+        }
+
+        internal SinglyNode GetLastNode(SinglyLinkedList singlyList)
+        {
+            SinglyNode temp = singlyList.head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+
+
+        internal void PrintList(SinglyLinkedList singlyList)
+        {
+            SinglyNode temp = singlyList.head;
+            while (temp.next != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+            Console.WriteLine(temp.data);
+            return;
+        }
+    }
+
+    internal class DoublyLinkedList
+    {
+        DoublyNode head = null;
+
+        internal void InsertLast(DoublyLinkedList doublyList, string newData)
+        {
+            DoublyNode newNode = new DoublyNode(newData);
+            if (doublyList.head == null)
+            {
+                newNode.prev = null;
+                doublyList.head = newNode;
+                return;
+            }
+            DoublyNode lastNode = GetLastNode(doublyList);
+            lastNode.next = newNode;
+            newNode.prev = lastNode;
+        }
+
+        internal DoublyNode GetLastNode(DoublyLinkedList doublyList)
+        {
+            DoublyNode temp = doublyList.head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+
+
+        internal void PrintList(DoublyLinkedList doublyList)
+        {
+            DoublyNode temp = doublyList.head;
+            while (temp.next != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+            Console.WriteLine(temp.data);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace LinkedListImplementations
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "us-cdbr-east-04.cleardb.com",
+                Database = "heroku_e65ea9d76424b15",
+                UserID = "bdfe4a1981ff43",
+                Password = "961e719de8d6de7",
+            };
+
+            using (var conn = new MySqlConnection(builder.ConnectionString))
+            {
+                Console.WriteLine("Opening connection");
+                await conn.OpenAsync();
+
+                using (var command = conn.CreateCommand())
+                {
+                    command.CommandText = "DROP TABLE IF EXISTS inventory;";
+                    await command.ExecuteNonQueryAsync();
+                    Console.WriteLine("Finished dropping table (if existed)");
+
+                    command.CommandText = "CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);";
+                    await command.ExecuteNonQueryAsync();
+                    Console.WriteLine("Finished creating table");
+
+                    command.CommandText = @"INSERT INTO inventory (name, quantity) VALUES (@name1, @quantity1),
+                        (@name2, @quantity2), (@name3, @quantity3);";
+                    command.Parameters.AddWithValue("@name1", "banana");
+                    command.Parameters.AddWithValue("@quantity1", 150);
+                    command.Parameters.AddWithValue("@name2", "orange");
+                    command.Parameters.AddWithValue("@quantity2", 154);
+                    command.Parameters.AddWithValue("@name3", "apple");
+                    command.Parameters.AddWithValue("@quantity3", 100);
+
+                    int rowCount = await command.ExecuteNonQueryAsync();
+                    Console.WriteLine(String.Format("Number of rows inserted={0}", rowCount));
+                }
+
+                // connection will be closed by the 'using' block
+                Console.WriteLine("Closing connection");
+            }
+        }
+    }
+}
+
+
+
